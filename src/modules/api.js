f74e484d742a61217ff25895285f4d01f452a7e9
Umut Sirin <umutsirin1@gmail.com>
2017-03-03 14:05:46 +0300

    Add support for multi-constructor response data to api module
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

--- src/modules/api.js
+++ src/modules/api.js
@@ -4,18 +4,37 @@ export const reducer = (state = immutable({}), action) => {
 
   const { result, error, meta = {} } = action
 
-  if (error || !meta.constructorName) {
+  if (error) {
     return state
   }
 
-  const { constructorName: name } = meta
+  if (meta.constructorNames) {
+    return handleMultiple(state, result, meta.constructorNames)
+  }
 
+  if (meta.constructorName) {
+    return handleSingle(state, result, meta.constructorName)
+  }
+
+  return state
+}
+
+const handleMultiple = (state, result, names) => {
+  Object.keys(names).forEach(key => {
+    const name = names[key]
+    const res = result[key]
+
+    state = handleSingle(state, result, name)
+  })
+
+  return state
+}
+
+const handleSingle = (state, result, name) => {
   const models = Array.isArray(result) ? result : [result]
-
   return models.reduce((state, model) => {
     // ensure we have a collection before updating.
     state = state[name] ? state : state.set(name, immutable({}))
-
     return state.update(name, col => col.set(model._id, immutable(model)))
   }, state)
 }
